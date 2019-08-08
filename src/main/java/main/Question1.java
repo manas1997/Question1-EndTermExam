f4d1b8c0013ece01c03815eeabe5b8bbbaf2bc43
Manas Mahapatra <Manas Mahapatra@DESKTOP-UC62JE2>
2019-08-08 13:51:38 +0530

    completed Sucessfully

--- src/main/java/main/Question1.java
+++ src/main/java/main/Question1.java
@@ -3,44 +3,44 @@ import java.util.Scanner;
 
 class Courses {
 	public String displayCourseDetails(String dept) {
-			
+
 			return "You have chosen "+dept+" department.\nThe common subjects are:\n1. Engineering Mathematics\n2. Physics\n3. Chemistry\n";
 		}
 }
 
-class ECE { //extend the parent class here 
+class ECE extends Courses{
 	public String displayCourseDetails(String dept) {
-		// write or edit your code here 
-		String commonSubjects = "";//call the parent function after using inheritence
-		String subject1 = ""; //write the specific subjects name here
-		String subject2 = ""; //write the specific subjects name here
-		
+        Courses c= new Courses();
+		String commonSubjects = c.displayCourseDetails(dept);//call the parent function after using inheritence
+		String subject1 = "Microprocessor"; //write the specific subjects name here
+		String subject2 = "Linear Integrated Circuits"; //write the specific subjects name here
+
 		String specificSubjects = " specific subjects are:\n4. "+subject1+"\n5. "+subject2;
 		return commonSubjects+dept+specificSubjects;
-		
+
 	}
 }
 class Mechanical { //extend the parent class here 
 	public String displayCourseDetails(String dept) {
-		// write or edit your code here 
-		String commonSubjects = "";//call the parent function after using inheritence
+		Courses c= new Courses(); 
+		String commonSubjects =c.displayCourseDetails(dept);//call the parent function after using inheritence
 		String subject1 = "Fluid Mechanics"; //write the specific subjects name here
 		String subject2 = "Thermodynamics"; //write the specific subjects name here
-		
+
 		String specificSubjects = " specific subjects are:\n4. "+subject1+"\n5. "+subject2;
 		return commonSubjects+dept+specificSubjects;	
-	
+
 	}
 
 }
 class CSE  { //extend the parent class here 
 	public String displayCourseDetails(String dept) {
-		
-		// write or edit your code here 
-		String commonSubjects = "";//call the parent function after using inheritence
-		String subject1 = ""; //write the specific subjects name here
-		String subject2 = ""; //write the specific subjects name here
-		
+
+		Courses c= new Courses(); 
+		String commonSubjects =c.displayCourseDetails(dept);//call the parent function after using inheritence
+		String subject1 = "Network Theory"; //write the specific subjects name here
+		String subject2 = "Operating Systems"; //write the specific subjects name here
+
 		String specificSubjects = " specific subjects are:\n4. "+subject1+"\n5. "+subject2;
 		return commonSubjects+dept+specificSubjects;		
 	}
@@ -57,27 +57,28 @@ public class Question1 {
 		switch(choice) {
 		case 1:
 		{
-			//this case is or ECE
-			//Initialize the Class using appropriately taking the concept of inheritence and polymorphism in mind.
-			System.out.println("");//call the function of the class that you initialized
+			ECE ece =new ECE();
+			System.out.println(ece.displayCourseDetails("ECE"));//call the function of the class that you initialized
 			break;
 		}
 		case 2:
 		{
 			//this case is or Mechanical
 			//Initialize the Class using appropriately taking the concept of inheritence and polymorphism in mind.
-			System.out.println("");//call the function of the class that you initialized
+			Mechanical mechanical =new Mechanical();
+			System.out.println(mechanical.displayCourseDetails("Mechanical"));//call the function of the class that you initialized
 			break;
 		}
 		case 3:
 		{
 			//this case is or CSE
 			//Initialize the Class using appropriately taking the concept of inheritence and polymorphism in mind.
-			System.out.println("");//call the function of the class that you initialized
+			CSE cse=new CSE();
+			System.out.println(cse.displayCourseDetails("CSE"));//call the function of the class that you initialized
 			break;
 		}
 		default:
-			System.out.println("Invalid input!");
+			System.out.println("");
 		break;
 		}
 	}
